07eae24919ddfdfb035b272507318f5d01a8adbf
pedroooliveira25 <eu.pedrohfo@gmail.com>
2026-05-15 09:21:02 -0300

    send changes

---
+++ src/Application/User/CreateUser.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+using Microsoft.VisualBasic;
+
+using Domain.
+public class CreateUser 
+{
+    //primeira coisa chamar o user para trabalharmos em cima dele.
+    public void CreateUser(string email, string name)
+    {
+    
+    }
+    
+}
