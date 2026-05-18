082af4256c63b4ccdca64a74625a13893088a36c
pedroooliveira25 <eu.pedrohfo@gmail.com>
2026-05-17 22:34:59 -0300

    Lapidando autentição simples do user

--- src/Domain/Entities/User/User.cs
+++ src/Domain/Entities/User/User.cs
@@ -7,19 +7,20 @@ public class User : Base
 {
     public string Email {get; private set;}
     public string Password {get; private set;}
+
+    public UserType userType {get; private set;}
       
-    public User(string name, string email, string password)
-    {   
+    public User(string name, string email, string password, UserType userType)
+    {   this.id = Guid.NewGuid(); 
+        this.Name = name; 
+        this.Email =email;
+        this.Password = password;
+
         ValidatePropertiesString(Email, "email");
         ValidatePropertiesString(name, "name");
         ValidatePropertiesString(Password, "Password");
-        ValidatePropertiesGuidId(id, "id" );
 
-
-        this.id = Guid.NewGuid(); 
-        this.Name = name; 
-        this.Email =email;
-        this.Password = password; 
+        this.userType = UserType.ADM; 
     }
 
 } 
